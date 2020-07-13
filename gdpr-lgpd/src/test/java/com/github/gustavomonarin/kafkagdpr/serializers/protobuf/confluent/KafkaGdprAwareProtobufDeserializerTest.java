cad8a5a8f828cdf621a37dc39f52ff66c7cac152
gustavomonarin <gustavomonarin@gmail.com>
2020-07-13 08:08:25 +0200

    tests for nullable records

--- gdpr-lgpd/src/test/java/com/github/gustavomonarin/kafkagdpr/serializers/protobuf/confluent/KafkaGdprAwareProtobufDeserializerTest.java
+++ gdpr-lgpd/src/test/java/com/github/gustavomonarin/kafkagdpr/serializers/protobuf/confluent/KafkaGdprAwareProtobufDeserializerTest.java
@@ -5,34 +5,53 @@ import com.acme.FruitOuterClass.Fruit;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializerConfig;
+import org.apache.kafka.common.header.internals.RecordHeaders;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class KafkaGdprAwareProtobufDeserializerTest {
 
     private final String topic = "test";
+    private final MockSchemaRegistryClient schemaRegistry = new MockSchemaRegistryClient();
+    private final Map<String, Object> configs;
+
+    public KafkaGdprAwareProtobufDeserializerTest() {
+        HashMap<String, Object> initial = new HashMap<>();
+        initial.put(KafkaProtobufSerializerConfig.AUTO_REGISTER_SCHEMAS, true);
+        initial.put(KafkaProtobufSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "bogus");
+        this.configs = Collections.unmodifiableMap(initial);
+    }
+
+    @Test
+    public void shouldSupportNullRecordReturningNullData(){
+        KafkaGdprAwareProtobufDeserializer<Fruit> deserializer = new KafkaGdprAwareProtobufDeserializer<>(
+                schemaRegistry,
+                configs,
+                Fruit.class
+        );
+
+        assertNull(deserializer.deserialize(topic, null));
+        assertNull(deserializer.deserialize(topic, new RecordHeaders(),null));
+    }
 
     @Test
     public void shouldBeCompatibleWithPlainProtobufSerializer() {
 
         Fruit preferredMelon = FruitFixture.waterMelon().build();
 
-        MockSchemaRegistryClient schemaRegistry = new MockSchemaRegistryClient();
-
-        HashMap<String, Object> configuration = new HashMap<>();
-        configuration.put(KafkaProtobufSerializerConfig.AUTO_REGISTER_SCHEMAS, true);
-        configuration.put(KafkaProtobufSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "bogus");
-
         KafkaProtobufSerializer<Fruit> serializer = new KafkaProtobufSerializer<>(
                 schemaRegistry,
-                configuration
+                configs
         );
         KafkaGdprAwareProtobufDeserializer<Fruit> deserializer = new KafkaGdprAwareProtobufDeserializer<>(
                 schemaRegistry,
-                configuration,
+                configs,
                 Fruit.class
         );
 
--- gdpr-lgpd/src/test/java/com/github/gustavomonarin/kafkagdpr/serializers/protobuf/confluent/KafkaGdprAwareProtobufSerializerTest.java
+++ gdpr-lgpd/src/test/java/com/github/gustavomonarin/kafkagdpr/serializers/protobuf/confluent/KafkaGdprAwareProtobufSerializerTest.java
@@ -5,35 +5,54 @@ import com.acme.FruitOuterClass.Fruit;
 import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializerConfig;
+import org.apache.kafka.common.header.internals.RecordHeaders;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class KafkaGdprAwareProtobufSerializerTest {
 
     private final String topic = "test";
+    private final MockSchemaRegistryClient schemaRegistry = new MockSchemaRegistryClient();
+    private final Map<String, Object> configs;
+
+    public KafkaGdprAwareProtobufSerializerTest() {
+        HashMap<String, Object> initial = new HashMap<>();
+        initial.put(KafkaProtobufSerializerConfig.AUTO_REGISTER_SCHEMAS, true);
+        initial.put(KafkaProtobufSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "bogus");
+        this.configs = Collections.unmodifiableMap(initial);
+    }
+
+    @Test
+    public void shouldSupportNullRecordReturningNullData(){
+        KafkaGdprAwareProtobufSerializer<Fruit> deserializer = new KafkaGdprAwareProtobufSerializer<>(
+                schemaRegistry,
+                configs,
+                Fruit.class
+        );
+
+        assertNull(deserializer.serialize(topic, null));
+        assertNull(deserializer.serialize(topic, new RecordHeaders(),null));
+    }
 
     @Test
     public void shouldBeCompatibleWithPlainProtobufDeserializer() {
 
         Fruit preferredMelon = FruitFixture.waterMelon().build();
 
-        MockSchemaRegistryClient schemaRegistry = new MockSchemaRegistryClient();
-
-        HashMap<String, Object> configuration = new HashMap<>();
-        configuration.put(KafkaProtobufSerializerConfig.AUTO_REGISTER_SCHEMAS, true);
-        configuration.put(KafkaProtobufSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "bogus");
-
         KafkaGdprAwareProtobufSerializer<Fruit> serializer = new KafkaGdprAwareProtobufSerializer<>(
                 schemaRegistry,
-                configuration,
+                configs,
                 Fruit.class
         );
         KafkaProtobufDeserializer<Fruit> deserializer = new KafkaProtobufDeserializer<>(
                 schemaRegistry,
-                configuration,
+                configs,
                 Fruit.class
         );
 
