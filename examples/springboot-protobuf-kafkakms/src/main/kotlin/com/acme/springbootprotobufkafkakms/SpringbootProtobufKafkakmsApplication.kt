345067df1323d709bb49ba3d3fcfea0e852dd3ad
gustavomonarin <gustavomonarin@gmail.com>
2020-09-24 14:28:49 +0200

    feat(example): Initial commit

---
+++ examples/springboot-protobuf-kafkakms/src/main/kotlin/com/acme/springbootprotobufkafkakms/SpringbootProtobufKafkakmsApplication.kt
@@ -0,0 +1,11 @@
+package com.acme.springbootprotobufkafkakms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootProtobufKafkakmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootProtobufKafkakmsApplication>(*args)
+}
---
+++ examples/springboot-protobuf-kafkakms/src/test/kotlin/com/acme/springbootprotobufkafkakms/SpringbootProtobufKafkakmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.acme.springbootprotobufkafkakms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootProtobufKafkakmsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
