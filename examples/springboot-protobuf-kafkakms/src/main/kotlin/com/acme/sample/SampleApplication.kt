0a58c2958200608284545b368d3605b4635473ee
gustavomonarin <gustavomonarin@gmail.com>
2020-10-09 14:10:46 +0200

    customize the retry back off
    By default the spring kafka listener will retry the failed messages immediately, which generates a lot of logs.
    
    This will configure to retry after 1.5 seconds, still showing errors. It should be quite safe as it should be smaller than the `max.poll.interval.ms` in most of cases.

--- examples/springboot-protobuf-kafkakms/src/main/kotlin/com/acme/sample/SampleApplication.kt
+++ examples/springboot-protobuf-kafkakms/src/main/kotlin/com/acme/sample/SampleApplication.kt
@@ -4,15 +4,28 @@ import org.apache.kafka.clients.admin.NewTopic
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.kafka.listener.ErrorHandler
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler
+import org.springframework.kafka.listener.SeekUtils
+import org.springframework.util.backoff.FixedBackOff
+
+
+fun main(args: Array<String>) {
+    runApplication<SampleApplication>(*args)
+}
+
 
 @SpringBootApplication
 class SampleApplication {
 
     @Bean
-    fun createKafkaTopic(): NewTopic = NewTopic("farmer", 3, 1)
+    fun kafkaErrorHandler(): ErrorHandler = SeekToCurrentErrorHandler(
+            FixedBackOff(1500, SeekUtils.DEFAULT_MAX_FAILURES.toLong()) // the default is to retry immediately
+    )
 
-}
-
-fun main(args: Array<String>) {
-    runApplication<SampleApplication>(*args)
+    @Bean
+    fun farmerTopic(): NewTopic = NewTopic(
+            "farmer",
+            3,
+            1)
 }
