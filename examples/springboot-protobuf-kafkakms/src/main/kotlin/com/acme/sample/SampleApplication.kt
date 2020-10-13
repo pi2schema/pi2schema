df776e5415a3714d63f9d364dd2d254f0a0a0e81
gustavomonarin <gustavomonarin@gmail.com>
2020-10-13 10:58:45 +0200

    Springboot kafkakms sample (#71)
    * feat(example): Initial commit
    
    * move docker compose to examples
    
    * initial sample without crypto shredding
    
    * customize the retry back off
    
    By default the spring kafka listener will retry the failed messages immediately, which generates a lot of logs.
    
    This will configure to retry after 1.5 seconds, still showing errors. It should be quite safe as it should be smaller than the `max.poll.interval.ms` in most of cases.
    
    * add crypto shredding using kafka backed keys
    
    * Add basic readme instructions
    
    * configure spotbugs to analyse only sample code
    
    Spotbugs by default inspects all the codebase, which includes auto generated protobuf code which is full of spotbugs warnings.

---
+++ examples/springboot-protobuf-kafkakms/src/main/kotlin/com/acme/sample/SampleApplication.kt
@@ -0,0 +1,47 @@
+package com.acme.sample
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.kafka.listener.ErrorHandler
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler
+import org.springframework.kafka.listener.SeekUtils
+import org.springframework.util.backoff.FixedBackOff
+
+
+fun main(args: Array<String>) {
+    runApplication<SampleApplication>(*args)
+}
+
+
+@SpringBootApplication
+class SampleApplication {
+
+    @Bean
+    fun kafkaErrorHandler(): ErrorHandler = SeekToCurrentErrorHandler(
+            FixedBackOff(1500, SeekUtils.DEFAULT_MAX_FAILURES.toLong()) // the default is to retry immediately
+    )
+
+    @Bean
+    fun farmerTopic(): NewTopic = NewTopic(
+            "farmer",
+            3,
+            1)
+
+    @Bean
+    fun pi2schemaCommandsTopic(): NewTopic = NewTopic(
+            "pi2schema.kms.commands",
+            3,
+            1)
+
+    @Bean
+    fun pi2schemaLocalStateTopic(): NewTopic = NewTopic(
+            "pi2schema.kms-local-changelog",
+            3,
+            1)
+            .configs(
+                    mapOf(
+                            "cleanup.policy" to "compact"))
+
+}
