a81bcb0827980694fa12d013c7e9b8c4bf314e02
Cristian Dugacicu <razorcd@yahoo.com>
2023-10-31 17:11:25 +0100

    enables tests and fix coverageReport

--- schema-providers-protobuf/src/test/java/pi2schema/schema/providers/protobuf/personaldata/PersonalMetadataProviderTest.java
+++ schema-providers-protobuf/src/test/java/pi2schema/schema/providers/protobuf/personaldata/PersonalMetadataProviderTest.java
@@ -1,61 +1,61 @@
-//package pi2schema.schema.providers.protobuf.personaldata;
-//
-//import com.acme.FarmerRegisteredEventFixture;
-//import com.acme.FruitFixture;
-//import com.acme.PlantaeOuterClass;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//class PersonalMetadataProviderTest {
-//
-//    PersonalMetadataProvider personalMetadataProvider;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        this.personalMetadataProvider = new PersonalMetadataProvider();
-//    }
-//
-//    @Test
-//    void givenADescriptorWithoutOneOfThenOptionalEmpty() {
-//
-//        var descriptorWithoutOneOf = FruitFixture.waterMelon().build().getDescriptorForType();
-//
-//        var metadata = personalMetadataProvider.forDescriptor(descriptorWithoutOneOf);
-//
-//        assertThat(metadata.requiresEncryption())
-//                .isFalse();
-//    }
-//
-//    @Test
-//    void givenADescriptorWithOneOfWithoutPersonalDataSiblingThenOptionalEmpty() {
-//
-//        var descriptorWithOneOf = PlantaeOuterClass.Plantae.newBuilder()
-//                .setFruit(FruitFixture.waterMelon())
-//                .build()
-//                .getDescriptorForType();
-//
-//        var metadata = personalMetadataProvider.forDescriptor(descriptorWithOneOf);
-//
-//        assertThat(metadata.requiresEncryption())
-//                .isFalse();
-//    }
-//
-//    @Test
-//    void givenADescriptorWithOneOfWithPersonalDataAsSiblingThenPersonalMetadata() {
-//        var descriptor = FarmerRegisteredEventFixture.johnDoe()
-//                .build()
-//                .getDescriptorForType();
-//
-//        var metadata = personalMetadataProvider.forDescriptor(descriptor);
-//
-//        assertThat(metadata.requiresEncryption())
-//                .isTrue();
-//    }
-//
-//    @Test
-//    void shouldCache() {
-////        fail();
-//    }
-//}
+package pi2schema.schema.providers.protobuf.personaldata;
+
+import com.acme.FarmerRegisteredEventFixture;
+import com.acme.FruitFixture;
+import com.acme.PlantaeOuterClass;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PersonalMetadataProviderTest {
+
+    PersonalMetadataProvider personalMetadataProvider;
+
+    @BeforeEach
+    public void setUp() {
+        this.personalMetadataProvider = new PersonalMetadataProvider();
+    }
+
+    @Test
+    void givenADescriptorWithoutOneOfThenOptionalEmpty() {
+
+        var descriptorWithoutOneOf = FruitFixture.waterMelon().build().getDescriptorForType();
+
+        var metadata = personalMetadataProvider.forDescriptor(descriptorWithoutOneOf);
+
+        assertThat(metadata.requiresEncryption())
+                .isFalse();
+    }
+
+    @Test
+    void givenADescriptorWithOneOfWithoutPersonalDataSiblingThenOptionalEmpty() {
+
+        var descriptorWithOneOf = PlantaeOuterClass.Plantae.newBuilder()
+                .setFruit(FruitFixture.waterMelon())
+                .build()
+                .getDescriptorForType();
+
+        var metadata = personalMetadataProvider.forDescriptor(descriptorWithOneOf);
+
+        assertThat(metadata.requiresEncryption())
+                .isFalse();
+    }
+
+    @Test
+    void givenADescriptorWithOneOfWithPersonalDataAsSiblingThenPersonalMetadata() {
+        var descriptor = FarmerRegisteredEventFixture.johnDoe()
+                .build()
+                .getDescriptorForType();
+
+        var metadata = personalMetadataProvider.forDescriptor(descriptor);
+
+        assertThat(metadata.requiresEncryption())
+                .isTrue();
+    }
+
+    @Test
+    void shouldCache() {
+//        fail();
+    }
+}
--- schema-providers-protobuf/src/test/java/pi2schema/schema/providers/protobuf/subject/SiblingSubjectIdentifierFinderTest.java
+++ schema-providers-protobuf/src/test/java/pi2schema/schema/providers/protobuf/subject/SiblingSubjectIdentifierFinderTest.java
@@ -1,60 +1,60 @@
-//package pi2schema.schema.providers.protobuf.subject;
-//
-//import com.acme.FarmerRegisteredEventFixture;
-//import com.acme.InvalidSubjectIdentifiers;
-//import org.junit.jupiter.api.Test;
-//import pi2schema.schema.subject.SubjectIdentifierNotFoundException;
-//import pi2schema.schema.subject.TooManySubjectIdentifiersException;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-//
-//class SiblingSubjectIdentifierFinderTest {
-//
-//    private final SiblingSubjectIdentifierFinder finder = new SiblingSubjectIdentifierFinder();
-//
-//    @Test
-//    void shouldFindTheSubjectIdentifierInAValidEvent() {
-//        var johnDoeRegistration = FarmerRegisteredEventFixture.johnDoe();
-//        var oneOfDescriptor = johnDoeRegistration
-//                .getDescriptorForType()
-//                .getOneofs()
-//                .get(0);
-//
-//        var subjectIdentifierFieldDefinition = finder.find(oneOfDescriptor);
-//
-//        assertThat(subjectIdentifierFieldDefinition).isNotNull();
-//        assertThat(subjectIdentifierFieldDefinition.subjectFrom(johnDoeRegistration))
-//                .isEqualTo(johnDoeRegistration.getUuid());
-//    }
-//
-//    @Test
-//    void shouldThrowSubjectIdentifierNotFound() {
-//        var personalDataField = InvalidSubjectIdentifiers.MissingSubjectIdentifierAnnotation
-//                .getDescriptor()
-//                .getOneofs().get(0);
-//
-//        assertThatExceptionOfType(SubjectIdentifierNotFoundException.class)
-//                .isThrownBy(() ->
-//                        finder.find(personalDataField)
-//                )
-//                .withMessage("The strategy pi2schema.schema.providers.protobuf.subject.SiblingSubjectIdentifierFinder has not found any possible identifiers for the field" +
-//                        " com.acme.MissingSubjectIdentifierAnnotation.personal_data while exact one is required");
-//
-//    }
-//
-//    @Test
-//    public void shouldThrowTooManySubjectIdentifiers() {
-//        var personalDataField = InvalidSubjectIdentifiers.MultipleSiblingsSubjectIdentifiers
-//                .getDescriptor()
-//                .getOneofs()
-//                .get(0);
-//
-//        assertThatExceptionOfType(TooManySubjectIdentifiersException.class)
-//                .isThrownBy(() ->
-//                        finder.find(personalDataField)
-//                )
-//                .withMessage("The strategy SiblingSubjectIdentifierFinder has found 2 possible identifiers for the field" +
-//                        " com.acme.MultipleSiblingsSubjectIdentifiers.personal_data while exact one is required");
-//    }
-//}
+package pi2schema.schema.providers.protobuf.subject;
+
+import com.acme.FarmerRegisteredEventFixture;
+import com.acme.InvalidSubjectIdentifiers;
+import org.junit.jupiter.api.Test;
+import pi2schema.schema.subject.SubjectIdentifierNotFoundException;
+import pi2schema.schema.subject.TooManySubjectIdentifiersException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class SiblingSubjectIdentifierFinderTest {
+
+    private final SiblingSubjectIdentifierFinder finder = new SiblingSubjectIdentifierFinder();
+
+    @Test
+    void shouldFindTheSubjectIdentifierInAValidEvent() {
+        var johnDoeRegistration = FarmerRegisteredEventFixture.johnDoe();
+        var oneOfDescriptor = johnDoeRegistration
+                .getDescriptorForType()
+                .getOneofs()
+                .get(0);
+
+        var subjectIdentifierFieldDefinition = finder.find(oneOfDescriptor);
+
+        assertThat(subjectIdentifierFieldDefinition).isNotNull();
+        assertThat(subjectIdentifierFieldDefinition.subjectFrom(johnDoeRegistration))
+                .isEqualTo(johnDoeRegistration.getUuid());
+    }
+
+    @Test
+    void shouldThrowSubjectIdentifierNotFound() {
+        var personalDataField = InvalidSubjectIdentifiers.MissingSubjectIdentifierAnnotation
+                .getDescriptor()
+                .getOneofs().get(0);
+
+        assertThatExceptionOfType(SubjectIdentifierNotFoundException.class)
+                .isThrownBy(() ->
+                        finder.find(personalDataField)
+                )
+                .withMessage("The strategy pi2schema.schema.providers.protobuf.subject.SiblingSubjectIdentifierFinder has not found any possible identifiers for the field" +
+                        " com.acme.MissingSubjectIdentifierAnnotation.personal_data while exact one is required");
+
+    }
+
+    @Test
+    public void shouldThrowTooManySubjectIdentifiers() {
+        var personalDataField = InvalidSubjectIdentifiers.MultipleSiblingsSubjectIdentifiers
+                .getDescriptor()
+                .getOneofs()
+                .get(0);
+
+        assertThatExceptionOfType(TooManySubjectIdentifiersException.class)
+                .isThrownBy(() ->
+                        finder.find(personalDataField)
+                )
+                .withMessage("The strategy SiblingSubjectIdentifierFinder has found 2 possible identifiers for the field" +
+                        " com.acme.MultipleSiblingsSubjectIdentifiers.personal_data while exact one is required");
+    }
+}
