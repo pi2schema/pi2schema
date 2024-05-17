8f0265aba0a7de430697ed321b2b49511da6e6c9
gustavomonarin <gustavomonarin@gmail.com>
2024-05-17 21:32:40 +0200

    tests

--- serialization-adapters-kafka/src/main/java/pi2schema/serialization/kafka/materials/InMemoryMaterialsProvider.java
+++ serialization-adapters-kafka/src/main/java/pi2schema/serialization/kafka/materials/InMemoryMaterialsProvider.java
@@ -4,10 +4,13 @@ import pi2schema.crypto.providers.inmemorykms.InMemoryKms;
 
 import java.util.Map;
 
+/** An in memory materials provider for testing purposes. */
 public class InMemoryMaterialsProvider implements MaterialsProviderFactory {
+
+    public static final InMemoryKms SINGLETON = new InMemoryKms();
 
     @Override
     public Object create(Map<String, ?> configs) {
-        return new InMemoryKms();
+        return SINGLETON;
     }
 }
