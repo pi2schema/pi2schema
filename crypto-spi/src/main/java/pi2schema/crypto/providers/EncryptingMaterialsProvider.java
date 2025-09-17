0234e6f703c7468a2e07bc4fc0e5367e8bab37d6
gustavomonarin <gustavomonarin@gmail.com>
2025-09-17 16:07:23 +0200

    feat: google tink

--- crypto-spi/src/main/java/pi2schema/crypto/providers/EncryptingMaterialsProvider.java
+++ crypto-spi/src/main/java/pi2schema/crypto/providers/EncryptingMaterialsProvider.java
@@ -1,12 +1,10 @@
 package pi2schema.crypto.providers;
 
-import pi2schema.crypto.materials.SymmetricMaterial;
-
 import java.io.Closeable;
 import java.util.concurrent.CompletableFuture;
 
 public interface EncryptingMaterialsProvider extends Closeable {
-    CompletableFuture<SymmetricMaterial> encryptionKeysFor(String subjectId);
+    CompletableFuture<EncryptionMaterial> encryptionKeysFor(String subjectId);
 
     @Override
     default void close() {}
